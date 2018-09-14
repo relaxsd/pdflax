604ce1fe72f8d6fbc994391456bd02401368700d
Martijn Kriek <martijn.kriek@relax-software.nl>
2018-09-14 16:46:07 +0200

    Added some Creator option constants.

--- src/Pdflax/Contracts/PdfCreator.php
+++ src/Pdflax/Contracts/PdfCreator.php
@@ -5,13 +5,23 @@ namespace Pdflax\Contracts;
 interface PdfCreator
 {
 
+    const ORIENTATION_PORTRAIT = 'portrait';
+    const ORIENTATION_LANDSCAPE = 'landscape';
+
+    const UNIT_PT = 'pt';
+    const UNIT_MM = 'mm';
+    const UNIT_CM = 'cm';
+    const UNIT_INCH = 'inch';
+
+    const SIZE_A4 = 'A4';
+
     /**
      * Create a PDF document
      *
-     * @param array|null $data
+     * @param array|null $options
      *
      * @return PdfDocumentInterface
      */
-    public function create($data = []);
+    public function create($options = []);
 
 }
